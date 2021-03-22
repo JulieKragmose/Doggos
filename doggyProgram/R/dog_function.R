164bbd68340de9f78e66fc26a8a6ef83a7c95e3c
JulieKragmose <s171220@student.dtu.dk>
2021-03-22 13:19:40 +0000

    Committing the doggy program.

---
+++ doggyProgram/R/dog_function.R
@@ -0,0 +1,14 @@
+#' A dog function
+#'
+#' This functions allows you to tell the world how much you love dogs.
+#' @param love Do you love dogs? Defaults to TRUE, because you should.
+#' @keywords dogs
+#' @export
+dog_function <- function(love=TRUE){
+  if(live==TRUE){
+    print("Dogs are the best! People suck!")
+  }
+  else{
+    print("I am a disgusting person.")
+  }
+}
